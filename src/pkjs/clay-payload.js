059ebee2010cf7857afbc44a4d081739d4d5c6a7
Toasbi <tk.feed@pm.me>
2026-07-02 14:33:05 +0200

    feat(clay): send healthMode as CLAY_HEALTH_MODE enum

--- src/pkjs/clay-payload.js
+++ src/pkjs/clay-payload.js
@@ -54,7 +54,7 @@ function buildClayPayload(settings, watchInfo, now) {
         })(),
         "CLAY_COLOR_TIME": settings.hasOwnProperty('colorTime') ? settings.colorTime : DEFAULT_COLOR_WHITE,
         "CLAY_DAY_NIGHT_SHADING": settings.hasOwnProperty('dayNightShading') ? settings.dayNightShading : true,
-        "CLAY_HEALTH_ENABLED": settings.hasOwnProperty('healthEnabled') ? settings.healthEnabled : true,
+        "CLAY_HEALTH_MODE": ['off', 'status', 'all'].indexOf(settings.healthMode || 'off'),
         "CLAY_FETCH_INTERVAL_MIN": parseInt(settings.fetchIntervalMin, 10) || 30,
         "CLAY_RAIN_COUNTDOWN_HORIZON": (function() {
             var rc = parseInt(settings.rainCountdownHorizon, 10);
